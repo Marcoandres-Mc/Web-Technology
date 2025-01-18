9d659ea1e896a9344f2d1530dbbfa409e191b6bc
Marcoandres <marcoandresha@gmail.com>
2025-01-18 12:15:50 -0500

    urls cambiados

--- client/src/api/urlAPI.js
+++ client/src/api/urlAPI.js
@@ -1,3 +1,3 @@
-export const API="https://web-technology-hyee.onrender.com";  
+export const API=  'http://localhost:3500'
 
-//'http://localhost:3500'
+//"https://web-technology-hyee.onrender.com";
