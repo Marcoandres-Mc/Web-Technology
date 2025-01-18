2d0de4f1355b7818630787846206981c400ee25a
Marcoandres <marcoandresha@gmail.com>
2025-01-18 12:31:35 -0500

    cambio de urls

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
     res.send('Conectado');
 })
 app.use(cors({
-    origin: process.env.MAIN_PAGE.replace(/\/$/, ''), //'http://localhost:5173'
+    origin: process.env.MAIN_PAGE.replace(/\/$/, ''), // 'http://localhost:5173'
     credentials: true
 }));
 
